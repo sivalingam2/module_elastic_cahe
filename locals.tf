69967b50888d22dec0fc13d72e2835a4a7d22500
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-13 11:12:32 +0530

    setting mongodb cluster

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
   name_prefix = "${var.env}-${var.elasticcache_type}-elasticcache"
-  tags        = merge(var.tags, {tf-module-name = elasticcache}, { env = var.env})
+  tags        = merge(var.tags, {tf-module-name = "elasticcache"}, { env = var.env})
 }
