70a41ffe97bc9622e6879195d0e6fbc12549c756
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-13 20:55:24 +0530

    setting mongodb cluster

--- main.tf
+++ main.tf
@@ -41,6 +41,6 @@ resource "aws_elasticache_cluster" "example" {
   parameter_group_name = aws_docdb_cluster_parameter_group.main.name
   engine_version       = var.engine_version
   port                 = var.port
-  db_subnet_group_name             = aws_elasticache_subnet_group.main.name
-  vpc_security_group_ids           = [aws_security_group.main.id]
+  subnet_group_name             = aws_elasticache_subnet_group.main.name
+  security_group_ids           = [aws_security_group.main.id]
 }
